d2afbb6d3c50432a2177ad5b57c91c1e60b4923d
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2022-03-22 12:29:16 +0530

    Add files via upload

---
+++ Addition Under Modulo GFG.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        long long sumUnderModulo(long long a,long long b)
+    {
+        long long int res = (pow(10,9)+7);
+        return (a%res+b%res)%res;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    cin>>a>>b;
+	    Solution ob;
+	    cout<<ob.sumUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
