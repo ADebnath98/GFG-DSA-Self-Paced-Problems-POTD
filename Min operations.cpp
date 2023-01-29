d39e0fac664bb7921de4ee1f02806ff24b3e0c43
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-01-29 11:45:36 +0530

    Add files via upload

---
+++ Min operations.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int solve(int a, int b) {
+        int ans = a & b;
+        int cnt = 0;
+        if(a != ans) 
+        cnt++;
+        if(b != ans) 
+        cnt++;
+        return cnt;
+}
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.solve(a, b);
+        
+        printf("%d\n", res);
+        
+    }
+}
+
+// } Driver Code Ends
