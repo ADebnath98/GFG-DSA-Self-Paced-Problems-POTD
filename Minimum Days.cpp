0f90da7b9bac6a89d03e0ae9b2aef3445356f5e1
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-02-11 11:00:00 +0530

    Add files via upload

---
+++ Minimum Days.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMinimumDays(int N,string S, vector<int> &P) {
+      int ans=0;
+        for(int i=0;i<N;i++) {
+            int idx = P[i];
+                 if((idx>0 and S[idx]==S[idx-1]) || (idx+1<N and S[idx]==S[idx+1])) {
+                    S[idx]='?';
+                    ans = i+1;
+                }
+        }
+        return ans;
+    } 
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        string S;cin>>S;
+        
+        vector<int> P(N);
+        Array::input(P,N);
+        
+        Solution obj;
+        int res = obj.getMinimumDays(N,S, P);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
