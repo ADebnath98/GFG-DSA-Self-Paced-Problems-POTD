c784f6ddf12a0a69f76e750012fa59f1248f199f
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-02-23 15:58:38 +0530

    Add files via upload

---
+++ Unique Paths in a Grid.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        vector<vector<int>> dp(n,vector<int> (m,0));
+        int x=1e9+7;
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    if(i-1>=0)
+                        dp[i][j]=(dp[i][j]+dp[i-1][j])%x;
+                    if(j-1>=0)
+                        dp[i][j]=(dp[i][j]+dp[i][j-1])%x;
+                }
+            }
+        }
+        return dp[n-1][m-1]%x;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m,x;
+        cin>>n>>m;
+        
+        vector<vector<int>> grid(n,vector<int>(m));
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                cin>>grid[i][j];
+            }
+        }
+
+        Solution ob;
+        cout << ob.uniquePaths(n,m,grid) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
