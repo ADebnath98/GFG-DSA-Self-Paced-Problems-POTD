112a759a6fd21abc1526718433daab8c189310af
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-02-28 18:24:06 +0530

    Add files via upload

---
+++ Optimal Array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<int> optimalArray(int n,vector<int> &a){
+
+        vector<int>ans(n,0);
+
+        int i=1;
+
+        while(i<n){
+
+            ans[i] = ans[i-1] + (a[i] - a[i/2]);
+
+            i++;
+
+        }
+
+        return ans;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        Solution obj;
+        vector<int> ans=obj.optimalArray(n,a);
+        for(auto ele:ans){
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
