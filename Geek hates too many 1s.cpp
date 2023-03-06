c0adb87d26ee37c574edb9430856655cc82967fc
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-03-06 12:53:20 +0530

    Add files via upload

---
+++ Geek hates too many 1s.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+
+  public:
+    int noConseBits(int n) {
+        // code here
+        int bits = 0;
+        for(int i=30;i>=0;i--)
+        {
+            if(((1<<i)&n)>0) bits++;
+            else bits=0;
+            if(bits==3)
+                n-=(1<<i),bits=0;
+        }
+        return n;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+
+        int n;
+        cin >> n;
+        int ans = sol.noConseBits(n);
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
