270059357d794b57f2d49d409935d2fe41570d85
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-05-03 00:36:20 +0530

    Add files via upload

---
+++ Make Palindrome.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    bool makePalindrome(int n,vector<string> &arr){
+        // Code here
+        unordered_map<string,int>m,p;
+        for(auto i:arr){
+            m[i]++;
+        reverse(i.begin(),i.end());
+        p[i]++;
+        }
+        return m==p;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        if(ob.makePalindrome(n,arr)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
