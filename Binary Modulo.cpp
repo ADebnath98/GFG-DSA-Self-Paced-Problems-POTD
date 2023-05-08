a5ee7c697a342f3a517083165ca99c999d68d2b3
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-05-08 19:11:50 +0530

    Add files via upload

---
+++ Binary Modulo.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        int modulo(string s,int m)
+        {
+            int ans=0,temp=1,i=s.size()-1;
+            while(i>=0){
+                if(s[i--]=='1')ans=(ans+temp)%m;
+                temp=(temp*2)%m;
+            }return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        int m;
+        cin>>s>>m;
+        Solution a;
+        cout<<a.modulo(s,m)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
