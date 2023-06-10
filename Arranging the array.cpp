d5517ac9565bce97ed3ce5038df4adfe70aff31a
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-06-10 15:07:26 +0530

    Add files via upload

---
+++ Arranging the array.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            vector<int> neg;
+            vector<int> pos;
+            for(int i = 0 ; i<n ; i++){
+                if(arr[i]<0){
+                    neg.push_back(arr[i]);
+                }else{
+                    pos.push_back(arr[i]);
+                }
+            }
+            
+            
+            for(int i = 0 ; i<neg.size();i++){
+                arr[i]=neg[i];
+            }
+            for(int i = 0 ; i<pos.size();i++){
+                arr[neg.size()+i]=pos[i];
+            }
+            
+        
+            // Your code goes here
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
