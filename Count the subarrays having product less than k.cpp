f1be40378c49f4b7dce671f47eef1c97b30adae6
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-07-04 12:14:19 +0530

    Add files via upload

---
+++ Count the subarrays having product less than k.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& arr, int n, long long k) {
+        long long  product = 1;
+        int count = 0;
+        int i = 0;
+        int j = 0;
+    
+        if (k <= 1) {
+            return 0;
+        }
+        while (j < n) {
+            product *= arr[j];
+    
+            while (product >= k ) {
+                product /= arr[i];
+                i++;
+            }
+            count += (j - i + 1);
+            j++;
+        }
+    
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
