24c176bb085c9ef61251c924cdac289506690ed8
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-08-05 00:18:50 +0530

    Add files via upload

---
+++ Chocolate Distribution Problem.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+        sort(a.begin(),a.end());
+        long long ans=1e9;
+        long long i=0;
+        long long j=m-1;
+        
+        while(j<n)
+        {
+            ans=min(ans,a[j]-a[i]);
+            i++;
+            j++;
+        }
+        
+        return ans;
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
