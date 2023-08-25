38d6b751f6d51f956e288425f8f160784790ab94
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-08-25 15:32:08 +0530

    Add files via upload

---
+++ Palindrome String.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int start=0,end=S.length()-1;
+        while(start<=end){
+            if(S[start]==S[end]){
+                start++;
+                end--;
+            }
+            else{
+                return 0;
+            }
+        }
+        return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
