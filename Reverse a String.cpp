b7e0b853e1b4eb277c3a4489c1216b3ade48b635
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-08-27 00:24:38 +0530

    Add files via upload

---
+++ Reverse a String.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        int low=0;
+        int high=str.size()-1;
+        while(low<high){
+            swap(str[low],str[high]);
+            low++;
+            high--;
+        }
+        return str;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
