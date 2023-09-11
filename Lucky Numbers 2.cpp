1b0644900e22e00bacbba405847318cf760f458b
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-09-11 18:37:34 +0530

    Add files via upload

---
+++ Lucky Numbers 2.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User-function template for C++
+
+// Complete the function
+// n: Input n
+// Return True if the given number is a lucky number else return False
+
+class Solution{
+public:
+    bool isLucky(int n) {
+    int i=2;
+    int k=n;
+    while(i<=k){
+        if(k%i==0){
+            return 0;
+            
+        }
+        // cout<<k<<" "<<i<<endl;
+        k-=(k/i);
+        i++;
+    }
+    return 1;
+    }
+};
+
+//{ Driver Code Starts.
+signed main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling isLucky() function
+        if(obj.isLucky(n))
+            cout<<"1\n";//printing "1" if isLucky() returns true
+        else
+            cout<<"0\n";//printing "0" if isLucky() returns false
+    }
+    
+}
+// } Driver Code Ends
