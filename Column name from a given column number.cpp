ecd47b9c323abbf9e57c6d72377bdeb5e477aec6
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-10-03 14:29:24 +0530

    Add files via upload

---
+++ Column name from a given column number.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string res="";
+        while(n)
+        {
+            n--;
+            int mod=n%26;
+            res=char(65+mod)+res;
+            n=n/26;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+
+// } Driver Code Ends
